43a5662137613567d2bd991f8effb7a01c4e0603
priyanjal1234 <priyanjal362@gmail.com>
2024-10-28 14:12:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://learning-deployment-frontend.onrender.com',
     credentials: true
 }))
 
